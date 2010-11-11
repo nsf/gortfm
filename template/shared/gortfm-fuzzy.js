af32b42f06e6d6f6b152e9b4c3d11150edf93711
nsf <no.smile.face@gmail.com>
2010-11-11 11:05:34 +0500

    Fix few corner cases in fuzzy matcher.
    For example:
    	crypto/sha256
    	crypto/sha512
    
    pattern 'csha5' was matching crypto/sha256 for some reason
    and pattern 'csha2' was matching crypto/sha512 :-D
    
    This fix changes fuzzy matching algorithm according to newly introduced
    description. Hopefully this time it will work fine.

--- template/shared/gortfm-fuzzy.js
+++ template/shared/gortfm-fuzzy.js
@@ -12,6 +12,59 @@
 // If score > 1 then it's less good.
 // 0 means no match.
 
+// _THE_ Algo:
+//
+// scores the match using the following scheme (* for match, > for shift):
+//-------------------------------------------------------------------------------
+// pat: AAA
+// str: BBAAA
+//
+// match: >>***
+//
+// 1 chunk matched:
+// 	chunk length: 3
+// 	match position: 3 (2+1)
+//
+// Score for this chunk is:
+// 	each letter base score (BS) = match position
+// 	chunk score (CS) = chunk length * BS
+//
+// 	CS == 9
+//
+//-------------------------------------------------------------------------------
+// pat: B
+// str: BBAAA
+//
+// 1 chunk match, length: 1, pos: 1
+// 	CS == 1 (but in reality == 2, 1 is reserved for exact match)
+//-------------------------------------------------------------------------------
+// Let's compare two near matches:
+// pat:  aaa
+// str1: BBaaBa
+// str2: BBaBaa
+//
+// str1 == 12
+// 	>>**>*
+// 	chunk1 == 6
+// 		pos: 3, length: 2, score: 3*2=6
+// 	chunk2 == 6
+// 		pos: 6, length: 1, score: 6*1=6
+//
+// 	chunk1 + chunk2 = 12
+//
+// str2 == 13
+// 	>>*>**
+// 	chunk1 == 3
+// 		pos: 3, length: 1, score: 3*1=3
+// 	chunk2 ==
+// 		pos: 5, length: 2, score: 5*2=10
+//
+// 	chunk1 + chunk2 = 13
+//
+// As you can see str1 is preferred over str2 because its score is closer to
+// zero.
+//-------------------------------------------------------------------------------
+
 function charEquals(a, b) {
 	return a.toLowerCase() == b.toLowerCase()
 }
@@ -52,7 +105,7 @@ function shiftAndMatchChunk(str, pat) {
 }
 
 function sequentialScore(str, pat) {
-	var score = 2
+	var score = 1
 	var matches = 0
 
 	var soff = 0
@@ -66,8 +119,7 @@ function sequentialScore(str, pat) {
 				return 1
 
 			matches += r.clen
-			if (r.shifts != 0)
-				score += r.shifts * r.clen * (poff+1)
+			score += r.clen * (r.shifts+soff+1)
 		}
 		soff += r.soff
 		poff += r.poff
